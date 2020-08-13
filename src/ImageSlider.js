ec837e9396ee8ccd27a0c12345d2f2522212d4ad
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-08-13 18:52:12 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,10 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+export default ImageSlider extends Component {
+  render() {
+    return(
+      <div></div>
+      )
+  }
+}
